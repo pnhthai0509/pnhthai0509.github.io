08f28bf3a5c54a91edc684211f50dc940d92a2eb
pnht <phannguyenhoangthai@gmail.com>
2024-09-24 05:30:19 +1000

    added button and play pause function

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,10 +1,28 @@
-const button1 = document.getElementById('play-button');
 let track1 = new Audio();
-track1.src = 'Audio/Assignment 2/Audio/p-hase_Dry-Down-feat-Ben-Snaath.mp3';
+track1.src = '/Assignment 2/Audio/p-hase_Dry-Down-feat-Ben-Snaath.mp3';
 
-button1.addEventListener('click', function(){
-    track1.play();
-    track1.addEventListener('playing', function(){
-        console.log('Track 1 is now playing!');
-    });
+const playPauseButton = document.querySelector("#play-pause-btn");
+const playPauseImg = document.querySelector("#play-pause-img");
+
+function toggle(){
+    if (track1.paused || track1.ended){
+        track1.play();
+        playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/pause--v1.png";
+        console.log('The music is now playing!');
+    }else {
+        track1.pause();
+        playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+        console.log('The music is stopped!');
+    }
+}
+
+playPauseButton.addEventListener("click", function(event){
+    event.stopPropagation();
+    toggle();
 });
+//Play button section
+const outside = document.querySelector("#container");
+outside.addEventListener('click', function(){
+    toggle();
+});
+//Click anywhere to play music section
