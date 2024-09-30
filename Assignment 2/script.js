965243b6f3a0d31802b7940ba63239b9ac62f7c5
pnht <phannguyenhoangthai@gmail.com>
2024-09-30 18:11:29 +1000

    abcxyz

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,39 +1,21 @@
-const trackContainer = document.querySelector('.track-container');
-const playBtn = document.querySelector('#play');
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
-const audio = document.querySelector('#audio');
-const progress = document.querySelector('.progress');
-const progressContainer = document.querySelector('.progress-contaner');
-const title = document.querySelector('.track-name');
-const cover = document.querySelector('.track-img');
+const upDownBtn = document.querySelector('#up-btn');
+const upDownImg = document.querySelector('.upDownImg');
+const trackListSection = document.querySelector('.track-list-section');
 
-const songs = [
-    'Dry Down feat Ben Snaath',
-    "He's",
-    'Leapt',
-    'Water Feature'
-];
 
-let songIndex = 1;
-
-//This is where the website will load the song and then update the song detail
-
-loadSong(songs[songIndex]);
-
-function loadSong(song) {
-    title.innerText = song //This will load the title of the song based on 
-    audio.src = `Assignment 2/Audio/${song}.mp3`
-    // cover.src = `images/${song}.jpg`
-}
-
-//Play pause button
-playBtn.addEventListener('click', togglePlay);
-
-function togglePlay() {
-    if (audio.paused || audio.ended) {
-        audio.play()
+if (upDownBtn && upDownImg) {
+  upDownBtn.addEventListener('click', function () {
+    if (upDownImg.classList.contains('fa-caret-up')) {
+      upDownImg.classList.remove('fa-caret-up');
+      upDownImg.classList.add('fa-caret-down');
+      trackListSection.classList.remove('track-list-section');
+      trackListSection.classList.add('track-list-section-active');
     } else {
-        audio.pause()
+      upDownImg.classList.remove('fa-caret-down');
+      upDownImg.classList.add('fa-caret-up');
+      trackListSection.classList.remove('track-list-section-active');
+      trackListSection.classList.add('track-list-section');
+
     }
+  });
 }
