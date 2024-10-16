88781e4815a7269b389cc0a045d44655103088f1
pnht <phannguyenhoangthai@gmail.com>
2024-10-16 14:21:47 +1100

    html/css/javascript foundation

---
+++ Assignment 3/script.js
@@ -0,0 +1,52 @@
+
+// const project2Btn = document.querySelector(".project-2");
+// const project3Btn = document.querySelector(".project-3");
+// const project4Btn = document.querySelector(".project-4");
+// const project5Btn = document.querySelector(".project-5");
+// const project6Btn = document.querySelector(".project-6");
+// const project7Btn = document.querySelector(".project-7");
+// const project8Btn = document.querySelector(".project-8");
+
+
+// const project1Btn = document.querySelector("#project1");
+// const project1Info = document.querySelector(".project-1-container");
+
+// project1Btn.addEventListener("click", toggleProject1);
+
+// document.addEventListener("click", handleDocumentClick);
+
+// // function toggleProject1() {
+// //     // Check if the active class is present
+// //     if (project1Info.classList.contains("project-1-container-active")) {
+// //         // Remove active class and add the default class
+// //         project1Info.classList.remove("project-1-container-active");
+// //         project1Info.classList.add("project-1-container");
+// //     } else {
+// //         // Remove default class and add the active class
+// //         project1Info.classList.remove("project-1-container");
+// //         project1Info.classList.add("project-1-container-active");
+// //     }
+// // }
+
+// // function handleDocumentClick() {
+// //     // Check if the click is outside the project1Info container
+// //     if (!project1Info.contains(event.target) && project1Info.classList.contains("project-1-container-active")) {
+// //         project1Info.classList.remove("project-1-container-active");
+// //         project1Info.classList.add("project-1-container");
+// //     }
+// // }
+
+
+// function toggleProject1(event) {
+//     event.stopPropagation(); // Prevent the event from bubbling up to the document
+//     project1Info.classList.toggle("project-1-container-active");
+//     project1Info.classList.toggle("project-1-container");
+// }
+
+// function handleDocumentClick(event) {
+//     // Check if the click is outside the project1Info container
+//     if (!project1Info.contains(event.target) && project1Info.classList.contains("project-1-container-active")) {
+//         project1Info.classList.remove("project-1-container-active");
+//         project1Info.classList.add("project-1-container");
+//     }
+// }
